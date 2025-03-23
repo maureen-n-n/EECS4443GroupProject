5f97b5fb1f588342b19278e2af87b1fa5d3d4e37
moblovescoding <79654365+maureen-n-n@users.noreply.github.com>
2025-03-22 21:24:18 -0400

    added project files

---
+++ app/src/main/java/com/example/eecs4443groupproject/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.eecs4443groupproject;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
