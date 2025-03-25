4d043680d554b934c6e8a07513af015c3285ac88
een <90470940+ihayork@users.noreply.github.com>
2025-03-25 17:23:51 -0400

    made main page, started on ui for it

---
+++ app/src/main/java/ca/yorku/eecs/mack/groupproject/main.java
@@ -0,0 +1,20 @@
+package ca.yorku.eecs.mack.groupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        init();
+    }
+
+    public void init() {
+
+    }
+}
